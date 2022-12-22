af6289685da6faf1fc01f05ceb6f94c6f36363d8
mariyamax <89139747+mariyamax@users.noreply.github.com>
2022-12-22 16:55:53 +0300

    Add files via upload

---
+++ MailSender/src/main/java/EmailUtil.java
@@ -0,0 +1,49 @@
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.sql.Date;
+
+public class EmailUtil {
+
+    /**
+     * Utility method to send simple HTML email
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("mmariyamaksimenko@gmail.com", "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse("mmariyamaksimenko@gmail.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
